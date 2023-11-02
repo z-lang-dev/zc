5dfa6060242aedb418f7a55a31abc6c54263a333
衍星居士 <visus@qq.com>
2023-11-02 12:10:24 +0800

    step3: implement simple interpreter

--- src/main.c
+++ src/main.c
@@ -1,11 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "zast.h"
 
 static void help(void) {
   printf("【用法】：./z <源码>|build <hello.z>|run <hello.z>\n");
 }
 
+// 打印AST
+static void print_ast(CallExpr *expr) {
+    printf("CallExpr {\n");
+    printf("  fn: %s\n", expr->fn);
+    printf("  arg: \"%s\"\n", expr->arg);
+    printf("}\n");
+}
+
+// 获取字符c在字符串中的位置
+int index_of(char *str, char c) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == c) {
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
+// 提取一个子串
+char *substr(char *str, int start, int end) {
+    int len = end - start;
+    char *result = calloc(len + 1, sizeof(char));
+    int i = 0;
+    while (i < len) {
+        result[i] = str[start + i];
+        i++;
+    }
+    result[i] = '\0';
+    return result;
+}
+
+// 解析表达式
+static CallExpr *parse_expr(char *code) {
+    printf("Parsing %s...\n", code);
+    // 解析源码
+    CallExpr *expr = calloc(1, sizeof(CallExpr));
+    // 从代码开头到'('之间的就是函数名称
+    int index_lparen = index_of(code, '(');
+    char *fn = substr(code, 0, index_lparen);
+    expr->fn = fn;
+    // 从'"'到'"'之间的就是参数
+    char *arg = substr(code, index_lparen + 2, strlen(code)-2);
+    expr->arg = arg;
+    // 打印出AST
+    print_ast(expr);
+    return expr;
+}
+
+// 执行AST
+static void execute(CallExpr *call) {
+    printf("Executing %s(%s)...\n", call->fn, call->arg);
+    // 打印call.arg
+    printf("%s\n", call->arg);
+}
+
+// 解释并执行代码
 static void interp(char *code) {
-    printf("TODO: interp %s\n", code);
+    printf("Interpreting %s...\n", code);
+    // 解析源码
+    CallExpr *call = parse_expr(code);
+    execute(call);
 }
 
 static void repl(void) {

6254adac0a0404634622da71ee0c6332b31ecfc6
衍星居士 <visus@qq.com>
2023-11-09 17:43:49 +0800

    补充提交步骤13的测试用例

---
+++ test/add_sub/add_sub_expected.asm
@@ -0,0 +1,9 @@
+includelib msvcrt.lib
+.code
+main proc
+    mov rax, 1
+    add rax, 5
+    sub rax, 3
+    ret
+main endp
+end
---
+++ test/add_sub/add_sub_expected.s
@@ -0,0 +1,8 @@
+    .intel_syntax noprefix
+    .text
+    .global main
+main:
+    mov rax, 1
+    add rax, 5
+    sub rax, 3
+    ret
